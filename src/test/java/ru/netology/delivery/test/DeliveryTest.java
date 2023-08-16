4c9616e59cc90ef1e7ad1f0980a7df26256e2fce
Neto test <irina.plotnikova72@mail.ru>
2023-08-16 14:02:26 +0500

    1

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -37,7 +37,7 @@ class DeliveryTest {
         $("[data-test-id='phone'] input").setValue(validUser.getPhone());
         $("[data-test-id='agreement']").click();
         $(byText("Запланировать")).click();
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(40));
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
         $("[data-test-id='success-notification'].notification__content")
                 .shouldHave(exactText("Встреча успешно забронирована на " + firstMeetingDate))
                 .shouldBe(visible);
